264f327863cf234e8b703db37b3a996a7c5e3a6a
Sparsh <sparshy@gmail.com>
2020-02-10 23:00:25 +0530

    implemented but not working stuck in loop, possible problem with interp1

---
+++ assignment2/equations_sym.m
@@ -0,0 +1,10 @@
+syms a M g Sw Cl Cd gamma Tw alpha lambda1 lambda2 t 
+denom = Sw * M^2 * Cl - cos(gamma) + Tw * sin(alpha);
+H = a * M / (g* denom) + lambda1 * M  *((- Sw * M^2 * Cd - sin(gamma) + Tw* cos(alpha))/denom) ...
+    + lambda2 * a* M /(g *denom);
+
+diff(H,M)
+diff(H,t)
+diff(H,lambda1)
+diff(H,lambda2)
+diff(H,alpha)
---
+++ assignment2/missile.m
@@ -0,0 +1,70 @@
+%Missile Guidance
+clc
+clear
+a = 343 ; % Speed of sound m/s
+rho = 1.22 ; % TODO : Need to update for 5kms
+mass = 240 ; %kg
+g = 9.8;
+S = 0.0707 ; 
+T = 24000  ; % Newton
+Tw = T / ( mass * g);
+Cd = 0.5   ;
+Cl = 3.12  ;
+Sw = rho* a^2 * S / ( 2 * mass* g);
+gamma = 0:-1:-180;
+rgamma = -180:1:0;
+alphat = gamma;
+q = 10;
+% Step  1 Assume the control history
+alpha = alphat * 0 ;
+
+for j=1:5
+% Step 2 Integrate the state equation forward
+[xt, x] = ode45( @(t,x) forward(t,x,alphat, alpha, Sw, Cl, Tw, Cd, a, g), gamma, [0.5 0] );
+
+% Step 3 Find final conditions and backward integrate the costate equation
+M_f = x(:,1);
+l1 = [q*(M_f(end)-0.8), 0];
+[lt,l] = ode45(@(t,l) backward(t,l, a,g,Cl, Sw, Tw, Cd, alphat, alpha, xt, x ), rgamma, l1);
+
+% Step 4 Verify if the control equation is satisfied
+M = x(:,1);
+lambda1 = flip(l(:,1));
+lambda2 = flip(l(:,2));
+dHdAlpha = (M*Tw.*lambda1.*cos(alpha').*(Cd*Sw*M.^2 + sin(gamma') - Tw*cos(alpha')))./...
+    (Cl*Sw*M.^2 - cos(gamma') + Tw*sin(alpha')).^2 - ...
+    (M*Tw*a.*cos(alpha'))./(g*(Cl*Sw*M.^2 - cos(gamma') + Tw*sin(alpha')).^2) - ...
+    (M*Tw.*lambda1.*sin(alpha'))./(Cl*Sw*M.^2 - cos(gamma') + Tw*sin(alpha')) - ...
+    (M*Tw*a.*lambda2.*cos(alpha'))./(g*(Cl*Sw*M.^2 - cos(gamma') + Tw*sin(alpha')).^2);
+tau = 0.1;
+plot(gamma, alpha);
+hold on
+alpha = alpha - tau * dHdAlpha';
+end
+
+% Plotting the optimal control
+%plot(time, -0.5806 * exp(time), 'r--')
+
+function dx = forward(gamma,x, alphat, alpha, Sw , Cl, Tw, Cd, a,g)
+alpha = interp1(alphat,alpha,gamma);
+M = x(1);
+denom = (Sw * M^2 * Cl - cos(gamma) + Tw * sin(alpha) );
+dx(1) = ( - Sw * M^2 * Cd - sin(gamma) + Tw * cos(gamma)) * M / denom;
+dx(2) = a * M / ( g * denom);
+dx = dx';
+end
+
+function dl = backward(gamma,l,a,g,Cl, Sw, Tw, Cd, alphat, alpha, xt, x)
+alpha = interp1(alphat, alpha, gamma);
+M = interp1(xt,x(:,1),gamma);
+lambda1 = l(1);
+lambda2 = l(2);
+dl(1) = a/(g*(Cl*Sw*M^2 - cos(gamma) + Tw*sin(alpha))) - (lambda1*(Cd*Sw*M^2 + sin(gamma) - Tw*cos(alpha)))/...
+    (Cl*Sw*M^2 - cos(gamma) + Tw*sin(alpha)) + (a*lambda2)/(g*(Cl*Sw*M^2 - cos(gamma) + Tw*sin(alpha))) - ...
+    (2*Cd*M^2*Sw*lambda1)/(Cl*Sw*M^2 - cos(gamma) + Tw*sin(alpha)) +...
+    (2*Cl*M^2*Sw*lambda1*(Cd*Sw*M^2 + sin(gamma) - Tw*cos(alpha)))/...
+    (Cl*Sw*M^2 - cos(gamma) + Tw*sin(alpha))^2 - (2*Cl*M^2*Sw*a)/(g*(Cl*Sw*M^2 - cos(gamma) +...
+    Tw*sin(alpha))^2) - (2*Cl*M^2*Sw*a*lambda2)/(g*(Cl*Sw*M^2 - cos(gamma) + Tw*sin(alpha))^2);
+dl(2) = 0;
+dl = dl';
+end
