e04b1a0145b567b09c247bce82dc92fdbf408e52
Sparsh <sparshy@gmail.com>
2020-02-12 10:40:30 +0530

    equations

--- assignment2/equations_sym.m
+++ assignment2/equations_sym.m
@@ -1,7 +1,7 @@
 syms a M g Sw Cl Cd gamma Tw alpha lambda1 lambda2 t 
 denom = Sw * M^2 * Cl - cos(gamma) + Tw * sin(alpha);
-H = a * M / (g* denom) + lambda1 * M  *((- Sw * M^2 * Cd - sin(gamma) + Tw* cos(alpha))/denom) ...
-    + lambda2 * a* M /(g *denom);
+H = (a * M) / (g* denom) + lambda1 * M  *((- Sw * M^2 * Cd - sin(gamma) + Tw* cos(alpha))/denom) ...
+    + (lambda2 * a* M) /(g *denom);
 
 diff(H,M)
 diff(H,t)
